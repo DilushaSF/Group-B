2dfaf85ea32666bc0e37c29c48e963586cac5023
Dilusha Sindutharu <122423554+DilushaSF@users.noreply.github.com>
2024-09-27 13:44:02 +0100

    Test js file commit

---
+++ test.js
@@ -0,0 +1,13 @@
+function getTest() {
+    let b = 1;
+}
+
+myFunction = () => {
+    inputText = document.getElementById("userInput").value;
+
+    if(inputText > 15){
+        alert("Laggage cannot be executed thatn 15Kg");
+    } else {
+        alert(inputText + "Kg")
+    }
+}

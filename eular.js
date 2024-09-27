674c7420a06d8aaefd9cb6a7b01310f670cadc49
Dilusha Sindutharu <122423554+DilushaSF@users.noreply.github.com>
2024-09-27 14:16:50 +0100

    commit 5

--- eular.js
+++ eular.js
@@ -1,14 +1,16 @@
 alert("eular.js is loaded");
 
+let a,b;
+
 functionSumOfMultiplies = () => {
     let sum = 0;
     for(let i = 0; i<1000; i++){
-        if(i % 3 == 0 || (i % 5 == 0)){
+        if(i % a == 0 || (i % b == 0)){
             sum += i;
         }
     }
 
-    alert("Sum of multiplies of 3 and 5 below 1000 : " + sum);
+    alert("Sum of multiplies of " + a + "and" + b + "below 1000 : " + sum);
 }
 
 functionSumOfMultiplies();
